fc30f272b1c9056de56fc461173c651206210cd1
khcho96 <khcho1492@gmail.com>
2026-01-12 17:04:17 +0900

    [level 1] Title: 달리기 경주, Time: 835.61 ms, Memory: 393 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -1,33 +1,26 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        List<String> playersList = new ArrayList<>(List.of(players));
         Map<String, Integer> indexs = new HashMap<>();
-        for (String player : players) {
-            indexs.put(player, playersList.indexOf(player));
+        for (int i = 0; i < players.length; i++) {
+            indexs.put(players[i], i);
         }
 
         for (String calling : callings) {
             int oldIndex = indexs.get(calling);
             int newIndex = oldIndex - 1;
 
-            String loser = playersList.get(newIndex);
+            String loser = players[newIndex];
 
-            playersList.set(oldIndex, loser);
-            playersList.set(newIndex, calling);
+            players[oldIndex] = loser;
+            players[newIndex] = calling;
 
             indexs.put(loser, oldIndex);
             indexs.put(calling, newIndex);
         }
 
-        String[] answer = new String[playersList.size()];
-        for (int i = 0; i < playersList.size(); i++) {
-            answer[i] = playersList.get(i);
-        }
-        return answer;
+        return players;
     }
 }
