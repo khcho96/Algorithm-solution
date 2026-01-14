f2ad34f7429e80cfb605ecceede168565ef56dc8
khcho96 <khcho1492@gmail.com>
2026-01-14 18:56:42 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.15 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int n) {
+        
+        if (n % 2 == 0) {
+            return "수박".repeat(n / 2);
+        }
+        
+        return "수박".repeat(n / 2) + "수";
+    }
+}
