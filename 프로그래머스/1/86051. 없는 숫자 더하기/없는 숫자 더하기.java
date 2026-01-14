c17c0d7c42c00b2b5bf37858706cbceb9b209700
khcho96 <khcho1492@gmail.com>
2026-01-14 12:06:58 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 3.05 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        List<Integer> list = Arrays.stream(numbers)
+                .boxed()
+                .collect(Collectors.toList());
+        for (int i = 0; i <= 9; i++) {
+            if (!list.contains(i)) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
