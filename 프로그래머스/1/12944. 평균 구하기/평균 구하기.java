4bf685a24e274549e4ec11f67acdca7dfd52a0cd
khcho96 <khcho1492@gmail.com>
2026-01-14 10:53:51 +0900

    [level 1] Title: 평균 구하기, Time: 3.19 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = Arrays.stream(arr)
+                .average()
+                .orElse(0);
+        return answer;
+    }
+}
