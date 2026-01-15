34a685e49a8819a8f0e939eb9159d8537322ad70
khcho96 <khcho1492@gmail.com>
2026-01-15 22:51:13 +0900

    [level 1] Title: 기사단원의 무기, Time: 638.42 ms, Memory: 97 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        for(int x = 1; x <= number; x++) {
+            System.out.print(dividerCount(x));
+            answer += dividerCount(x) > limit ? power : dividerCount(x);
+        }
+        return answer;
+    }
+    
+    public int dividerCount(int x) {
+        int cnt = 0;
+        for (int i = 1; i < Math.sqrt(x); i++) {
+            if (x % i == 0) cnt += 2;
+        }
+        if ((int) Math.sqrt(x) * Math.sqrt(x) == x) cnt++;
+        return cnt;
+    }
+}
