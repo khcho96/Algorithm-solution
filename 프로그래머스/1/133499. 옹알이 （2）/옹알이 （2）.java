07295f956f6fa7f537a0f44954e519c19d679df7
khcho96 <khcho1492@gmail.com>
2026-01-16 16:29:23 +0900

    [level 1] Title: 옹알이 (2), Time: 0.58 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,34 +1,22 @@
 class Solution {
     public int solution(String[] babbling) {
+        String[] possible = new String[]{"aya", "ye", "woo", "ma"};
         int answer = 0;
         for (String string : babbling) {
             String preMatch = "";
             while (true) {
-                if (string.startsWith("aya") && !preMatch.equals("aya")) {
-                    string = string.substring("aya".length());
-                    preMatch = "aya";
-                    continue;
+                String temp = preMatch;
+                for (int i = 0; i < 4; i++) {
+                    if (string.startsWith(possible[i]) && !preMatch.equals(possible[i])) {
+                        string = string.substring(possible[i].length());
+                        preMatch = possible[i];
+                        break;
+                    }
                 }
 
-                if (string.startsWith("ye") && !preMatch.equals("ye")) {
-                    string = string.substring("ye".length());
-                    preMatch = "ye";
-                    continue;
+                if (string.isBlank() || preMatch.equals(temp)) {
+                    break;
                 }
-
-                if (string.startsWith("woo") && !preMatch.equals("woo")) {
-                    string = string.substring("woo".length());
-                    preMatch = "woo";
-                    continue;
-                }
-
-                if (string.startsWith("ma") && !preMatch.equals("ma")) {
-                    string = string.substring("ma".length());
-                    preMatch = "ma";
-                    continue;
-                }
-                
-                break;
             }
             if (string.isBlank()) {
                 answer++;
