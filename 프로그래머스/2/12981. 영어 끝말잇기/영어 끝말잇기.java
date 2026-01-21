8e94270d87184314c764d206733160be177cd15b
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-01-21 21:53:56 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        Set<String> set = new HashSet<>();
+        set.add(words[0]);
+        char c = words[0].charAt(words[0].length() - 1);
+        for (int i = 1; i < words.length; i++) {
+            if (set.contains(words[i]) || c != words[i].charAt(0)) {
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
+                break;
+            }
+
+            set.add(words[i]);
+            c = words[i].charAt(words[i].length() - 1);
+        }
+        
+        return answer;
+    }
+}
