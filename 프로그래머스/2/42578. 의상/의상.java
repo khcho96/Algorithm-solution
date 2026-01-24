701a8676156616e8ad670139f230def6fa717b1a
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-01-24 12:54:06 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] clothe : clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
+        }
+
+        int answer = 1;
+        for (Integer value : map.values()) {
+            answer *= (value + 1);
+        }
+        
+        return answer - 1;
+    }
+}
