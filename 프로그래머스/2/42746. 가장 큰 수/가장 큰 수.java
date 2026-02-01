6746d6a63bfbdad92ead50e3622b0a3b0a43621b
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-02-01 14:03:55 +0900

    [level 2] Title: 가장 큰 수, Time: 202.11 ms, Memory: 130 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+
+    public static String solution(int[] numbers) {
+        String[] strings = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
+
+        Arrays.sort(strings, (a, b) -> (b + a).compareTo(a + b));
+        
+        if (strings[0].equals("0")) return "0";
+        return String.join("", strings);
+    }
+}
+
+/*
+두 문자열 a, b 를 비교할때 a + b랑 b + a를 비교하면 되는데 그 부분을 생각 못해서 AI 도움을 받음
+
+*/
