13212ba625c8dc72ff2270dec8c81d945561c151
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-02-03 16:42:44 +0900

    [level 2] Title: 큰 수 만들기, Time: 71.46 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -5,19 +5,11 @@ class Solution {
         Deque<Integer> stack = new ArrayDeque<>();
         for (int i = 0; i < number.length(); i++) {
             int num = Integer.parseInt(String.valueOf(number.charAt(i)));
-            while (!stack.isEmpty() && stack.peekLast() < num) {
+            while (k > 0 && !stack.isEmpty() && stack.peekLast() < num) {
                 stack.removeLast();
                 k--;
-                if (k == 0) break;
             }
             stack.addLast(num);
-
-            if (k == 0) {
-                for (int j = i + 1; j < number.length(); j++) {
-                    stack.addLast(Integer.parseInt(String.valueOf(number.charAt(j))));
-                }
-                break;
-            }
         }
 
         if (k > 0) {
