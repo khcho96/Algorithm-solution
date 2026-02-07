0b2f0c9a999f92df30d40bdc21c1f17005a6547c
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-02-07 21:42:45 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -1,6 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int n, long k) {
-        int[] answer = {};
+        int[] answer = new int[n];
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= n; i++) numbers.add(i);
+
+        long fac = 1;
+        for (int i = 2; i < n; i++) fac *= i;
+
+        int idx = 0;
+        int i = n - 1;
+        while (i > 0) {
+            int pick = (int)((k - 1) / fac);
+            answer[idx++] = numbers.remove(pick);
+            k = (k - 1) % fac + 1;
+            fac /= i;
+            i--;
+        }
+        answer[idx] = numbers.remove(0);
         return answer;
     }
 }
