3c730daa7f467ae60cede18a604ef8e0a0c43588
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-02-08 15:43:17 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 17.28 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public static int solution(int [][]board)
     {
@@ -7,24 +5,14 @@ class Solution {
         int[][] dp = new int[board.length][board[0].length];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 0) dp[i][j] = 0;
-                else {
-                    int one = (i - 1 >= 0) ? dp[i - 1][j] : 0;
-                    int two = (j - 1 >= 0) ? dp[i][j - 1] : 0;
-                    int three = (i - 1 >= 0 && j - 1 >= 0) ? dp[i - 1][j - 1] : 0;
+                if (board[i][j] == 0) continue;
 
-                    if (one == two && two == three) dp[i][j] = one + 1;
-                    else if (one == 0 || two == 0 || three == 0) dp[i][j] = 1;
-                    else {
-                        dp[i][j] = Math.min(one, two);
-                        if (Math.min(one, two) <= three) dp[i][j]++;
-                    }
-                }
-            }
-        }
+                int one = (i - 1 >= 0) ? dp[i - 1][j] : 0;
+                int two = (j - 1 >= 0) ? dp[i][j - 1] : 0;
+                int three = (i - 1 >= 0 && j - 1 >= 0) ? dp[i - 1][j - 1] : 0;
 
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
+                dp[i][j] = Math.min(Math.min(one, two), three) + 1;
+
                 answer = Math.max(answer, dp[i][j]);
             }
         }
