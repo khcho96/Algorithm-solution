21893d2a602faac84930177a4cb7465a8c151a6f
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-02-12 00:48:45 +0900

    [level 2] Title: 후보키, Time: 5.77 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,81 +1,41 @@
 import java.util.*;
 
 class Solution {
-
-    public int answer = 0;
-    public String[][] relations;
-
     public int solution(String[][] relation) {
-        relations = relation;
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < relation[0].length; i++) list.add(i);
-        boolean[] visited = new boolean[relation[0].length];
-        Set<Set<Integer>> candidateKeys = new HashSet<>();
-        List<Integer> cur = new ArrayList<>();
-        for (int i = 1; i <= relation[0].length; i++) {
-            func(visited, list, i, cur, candidateKeys, 0);
-        }
+        int row = relation.length;
+        int col = relation[0].length;
 
-        return answer;
-    }
+        List<Integer> candidateKeys = new ArrayList<>(); // 후보키를 비트마스크로 저장
 
-    private void func(boolean[] visited, List<Integer> list, int r, List<Integer> cur, Set<Set<Integer>> candidateKeys, int start) {
-        if (r == 0) {
-            List<Row> rows = new ArrayList<>();
+        for (int mask = 1; mask < (1 << col); mask++) {
 
-            for (String[] relation : relations) {
-                Row row = new Row();
-                for (int integer : cur) row.add(relation[integer]);
-                rows.add(row);
-            }
-
-            if (new HashSet<>(rows).size() == rows.size()) {
-                Set<Integer> set = new HashSet<>();
-                for (Integer integer : cur) set.add(integer);
-
-                for (Set<Integer> candidateKey : candidateKeys) {
-                    if (set.containsAll(candidateKey)) return;
+            // 1) 최소성 체크: 기존 후보키가 부분집합이면 스킵
+            boolean minimal = true;
+            for (int key : candidateKeys) {
+                if ((mask & key) == key) { // key ⊆ mask
+                    minimal = false;
+                    break;
                 }
+            }
+            if (!minimal) continue;
 
-                answer++;
-                candidateKeys.add(set);
+            // 2) 유일성 체크
+            Set<String> seen = new HashSet<>();
+            for (int r = 0; r < row; r++) {
+                StringBuilder sb = new StringBuilder();
+                for (int c = 0; c < col; c++) {
+                    if ((mask & (1 << c)) != 0) {
+                        sb.append(relation[r][c]).append('|'); // 구분자 필수
+                    }
+                }
+                seen.add(sb.toString());
             }
 
-            return;
-        }
-
-        for (Integer i = start; i < list.size(); i++) {
-            visited[i] = true;
-            cur.add(i);
-            func(visited, list, r - 1, cur, candidateKeys, i + 1);
-            cur.remove(i);
-            visited[i] = false;
-        }
-    }
-
-    public class Row {
-        List<String> contents;
-
-        public Row() {
-            this.contents = new ArrayList<>();
-        }
-
-        public void add(String content) {
-            contents.add(content);
-        }
-
-        @Override
-        public boolean equals(Object object) {
-            if (object == null || getClass() != object.getClass()) {
-                return false;
+            if (seen.size() == row) { // 모든 행이 구분됨
+                candidateKeys.add(mask);
             }
-            Row row = (Row) object;
-            return Objects.equals(contents, row.contents);
         }
 
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(contents);
-        }
+        return candidateKeys.size();
     }
 }
