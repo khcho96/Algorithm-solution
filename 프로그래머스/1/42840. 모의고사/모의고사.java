71ae5f743810a53409bf309e006cec867bdaab69
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-02-15 22:01:11 +0900

    [level 1] Title: 모의고사, Time: 4.16 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,28 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] cnt = new int[3];
-        int[] one = new int[]{1,2,3,4,5};
-        int[] two = new int[]{2,1,2,3,2,4,2,5};
-        int[] three = new int[]{3,3,1,1,2,2,4,4,5,5};
-        for (int i = 0; i < answers.length; i++) {
-            if(answers[i] == one[i % one.length]) cnt[0]++;
-            if(answers[i] == two[i % two.length]) cnt[1]++;
-            if(answers[i] == three[i % three.length]) cnt[2]++;
-        }
-        
-        int max = Arrays.stream(cnt)
-            .max()
-            .orElse(0);
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            if (max == cnt[i]) {
-                list.add(i + 1);
-            }
-        }
-        
-        return list.stream()
-            .mapToInt(Integer::intValue)
-            .toArray();
+        List<Integer> answer = new ArrayList<>();
+        int[][] list = new int[][]{{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int[] count = new int[3];
+
+        for (int i = 0; i < 3; i++) for (int j = 0; j < answers.length; j++) if (list[i][j % list[i].length] == answers[j]) count[i]++;
+
+        int max = Math.max(Math.max(count[0], count[1]), count[2]);
+        for (int i = 0; i < 3; i++) if (max == count[i]) answer.add(i + 1);
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
