518407b1f955480c7227697c240ce6642ffea472
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-02-18 12:30:19 +0900

    [level 2] Title: 방문 길이, Time: 4.00 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,41 +1,41 @@
 import java.util.*;
 
 class Solution {
+
     public int solution(String dirs) {
         int answer = 0;
-        Map<Coordinate, Set<Coordinate>> map = new HashMap<>();
-        Coordinate cur = new Coordinate(0, 0);
-        for (String string : dirs.split("")) {
-            Coordinate next;
-            if (string.equals("U")) {
-                next = cur.up();
-            } else if (string.equals("D")) {
-                next = cur.down();
-            } else if (string.equals("R")) {
-                next = cur.right();
-            } else {
-                next = cur.left();
+
+        Set<Set<Coordinate>> set = new HashSet<>();
+
+        int x = 0, y = 0;
+        for (String dir : dirs.split("")) {
+            if (dir.equals("U") && y + 1 <= 5) {
+                Set<Coordinate> coordinates = Set.of(new Coordinate(x, y), new Coordinate(x, y + 1));
+                if (!set.contains(coordinates)) answer++;
+                set.add(coordinates);
+                y++;
+            } else if (dir.equals("D") && y - 1 >= -5) {
+                Set<Coordinate> coordinates = Set.of(new Coordinate(x, y), new Coordinate(x, y - 1));
+                if (!set.contains(coordinates)) answer++;
+                set.add(coordinates);
+                y--;
+            } else if (dir.equals("L") && x - 1 >= -5) {
+                Set<Coordinate> coordinates = Set.of(new Coordinate(x, y), new Coordinate(x - 1, y));
+                if (!set.contains(coordinates)) answer++;
+                set.add(coordinates);
+                x--;
+            } else if (dir.equals("R") && x + 1 <= 5){
+                Set<Coordinate> coordinates = Set.of(new Coordinate(x, y), new Coordinate(x + 1, y));
+                if (!set.contains(coordinates)) answer++;
+                set.add(coordinates);
+                x++;
             }
-
-            if (next == null) continue;
-
-            if (!map.containsKey(cur) || !map.get(cur).contains(next)) {
-                if (map.containsKey(cur)) map.get(cur).add(next);
-                else map.put(cur, new HashSet<>(Set.of(next)));
-
-                if (map.containsKey(next)) map.get(next).add(cur);
-                else map.put(next, new HashSet<>(Set.of(cur)));
-
-                answer++;
-            }
-
-            cur = next;
         }
-        
+
         return answer;
     }
 
-    public static class Coordinate {
+    public class Coordinate {
         int x;
         int y;
 
@@ -56,34 +56,6 @@ class Solution {
         @Override
         public int hashCode() {
             return Objects.hash(x, y);
-        }
-
-        public Coordinate up() {
-            if (y + 1 > 5) {
-                return null;
-            }
-            return new Coordinate(x, y + 1);
-        }
-
-        public Coordinate down() {
-            if (y - 1 < -5) {
-                return null;
-            }
-            return new Coordinate(x, y - 1);
-        }
-
-        public Coordinate right() {
-            if (x + 1 > 5) {
-                return null;
-            }
-            return new Coordinate(x + 1, y);
-        }
-
-        public Coordinate left() {
-            if (x - 1 < -5) {
-                return null;
-            }
-            return new Coordinate(x - 1, y);
         }
     }
 }
