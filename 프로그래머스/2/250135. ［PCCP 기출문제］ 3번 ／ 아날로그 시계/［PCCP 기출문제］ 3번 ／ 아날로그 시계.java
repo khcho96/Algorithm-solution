fe84502471bccef60fb675e1ec497f078bdbb9bc
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-02-19 21:55:41 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 6.23 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
         int answer = 0;
-
         final int CYCLE = 3600 * 12;
         int start = h1 * 3600 + m1 * 60 + s1;
         int end = h2 * 3600 + m2 * 60 + s2;
