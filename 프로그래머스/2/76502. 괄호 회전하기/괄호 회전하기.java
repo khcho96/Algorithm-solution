15882e2680f9d4496cb0b207a958aad87656857c
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-02-24 23:03:04 +0900

    [level 2] Title: 괄호 회전하기, Time: 69.15 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,45 +1,34 @@
 import java.util.*;
 
 class Solution {
+
+    public Map<Character, Character> map = Map.of(')', '(', '}', '{', ']', '[');
+
     public int solution(String s) {
         int answer = 0;
-        Map<String, String> map = Map.of(
-                ")", "(",
-                "}", "{",
-                "]", "["
-        );
 
         StringBuilder stringBuilder = new StringBuilder(s);
         for (int i = 0; i < s.length(); i++) {
-            if (isRight(map, stringBuilder.toString())) answer++;
+            if (isCorrect(stringBuilder.toString())) answer++;
 
-            stringBuilder.append(stringBuilder.charAt(0));
-            stringBuilder.delete(0, 1);
+            char c = stringBuilder.charAt(0);
+            stringBuilder.deleteCharAt(0);
+            stringBuilder.append(c);
         }
 
         return answer;
     }
 
-    private static boolean isRight(Map<String, String> map, String string) {
-        Deque<String> deque = new ArrayDeque<>();
+    private boolean isCorrect(String string) {
+        Deque<Character> stack = new ArrayDeque<>();
         for (int i = 0; i < string.length(); i++) {
-            String value = String.valueOf(string.charAt(i));
+            char c = string.charAt(i);
 
-            if (map.containsValue(value)) { // open
-                deque.addLast(value);
-            } else { // close
-                if (deque.isEmpty()) {
-                    return false;
-                } else {
-                    if (deque.peekLast().equals(map.get(value))) {
-                        deque.removeLast();
-                    } else {
-                        return false;
-                    }
-                }
-            }
+            if (c == '(' || c == '{' || c == '[') stack.addLast(c);
+            else if (stack.isEmpty()) return false;
+            else if (map.get(c) != stack.removeLast()) return false;
         }
 
-        return deque.isEmpty();
+        return stack.isEmpty();
     }
 }
