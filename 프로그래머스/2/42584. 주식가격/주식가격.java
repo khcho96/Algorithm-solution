e27086cd190565fc06754aa6bdd6b0c3cb9fe55a
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-02-26 21:18:34 +0900

    [level 2] Title: 주식가격, Time: 37.04 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,27 +1,27 @@
 import java.util.*;
 
 class Solution {
+
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        answer[prices.length - 1] = 0;
-        Deque<Integer> notYet = new ArrayDeque<>();
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.add(0);
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i - 1] > prices[i]) {
-                answer[i - 1] = 1;
-                while (!notYet.isEmpty() && prices[notYet.peekLast()] > prices[i]) {
-                    int index = notYet.removeLast();
-                    answer[index] = i - index;
+            if (!stack.isEmpty() && prices[stack.peekLast()] <= prices[i]) stack.addLast(i);
+            else {
+                while (!stack.isEmpty() && prices[stack.peekLast()] > prices[i]) {
+                    Integer removed = stack.removeLast();
+                    answer[removed] = i - removed;
                 }
-            } else {
-                notYet.add(i - 1);
+                stack.addLast(i);
             }
         }
 
-        while (!notYet.isEmpty()) {
-            int index = notYet.removeLast();
-            answer[index] = (prices.length - 1) - index;
+        for (int i : stack) {
+            answer[i] = stack.peekLast() - i;
         }
-        
+
         return answer;
     }
 }
