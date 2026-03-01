c16900e96098ac9da8523ce2df3c905910421303
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-03-01 21:48:36 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.59 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,29 +1,34 @@
 import java.util.*;
 
 class Solution {
+
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
-        Deque<Integer> deque = new ArrayDeque<>();
+
+        Deque<Integer> stack = new ArrayDeque<>();
         for (int move : moves) {
-            int doll = 0;
-            for (int i = 0; i < board.length; i++) {
-                if (board[i][move - 1] != 0) {
-                    doll = board[i][move - 1];
-                    board[i][move - 1] = 0;
-                    break;
-                }
-            }
-            if (!deque.isEmpty()) {
-                if (deque.peekFirst() == doll) {
-                    deque.removeFirst();
-                    answer += 2;
-                } else {
-                    deque.addFirst(doll);
-                }
-            } else if (doll != 0) {
-                deque.addFirst(doll);
+            int doll = getDoll(board, move);
+            if (doll == 0) continue;
+
+            if (!stack.isEmpty() && stack.peekLast() == doll) {
+                answer += 2;
+                stack.removeLast();
+            } else {
+                stack.addLast(doll);
             }
         }
+
         return answer;
+    }
+
+    private int getDoll(int[][] board, int move) {
+        for (int[] ints : board) {
+            if (ints[move - 1] != 0) {
+                int temp = ints[move - 1];
+                ints[move - 1] = 0;
+                return temp;
+            }
+        }
+        return 0;
     }
 }
