25719cefbdf01424bf9e09254c243e7f3d4a872c
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-03-07 12:21:12 +0900

    [level 2] Title: 할인 행사, Time: 67.10 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,36 +1,31 @@
 import java.util.*;
 
 class Solution {
+
     public int solution(String[] want, int[] number, String[] discount) {
-        Map<String, Integer> wantCount = new HashMap<>();
-        for (int i = 0; i < want.length; i++) {
-            wantCount.put(want[i], number[i]);
-        }
-
-        Map<String, Integer> curCount = new HashMap<>();
-        for (int i = 0; i < 9; i++) {
-            curCount.put(discount[i], curCount.getOrDefault(discount[i], 0) + 1);
-        }
-
         int answer = 0;
-        for (int i = 9; i < discount.length; i++) {
-            curCount.put(discount[i], curCount.getOrDefault(discount[i], 0) + 1);
 
-            if (isAllSatisfied(wantCount, curCount)) {
-                answer++;
-            }
-            curCount.put(discount[i - 9], curCount.get(discount[i - 9]) - 1);
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            map.put(want[i], number[i]);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (map.containsKey(discount[i])) map.put(discount[i], map.get(discount[i]) - 1);
+        }
+
+        if (isPossible(new HashSet<>(map.values()))) answer++;
+        for (int i = 10; i < discount.length; i++) {
+            if (map.containsKey(discount[i - 10])) map.put(discount[i - 10], map.get(discount[i - 10]) + 1);
+            if (map.containsKey(discount[i])) map.put(discount[i], map.get(discount[i]) - 1);
+
+            if (isPossible(new HashSet<>(map.values()))) answer++;
         }
 
         return answer;
     }
 
-    private static boolean isAllSatisfied(Map<String, Integer> wantCount, Map<String, Integer> curCount) {
-        for (String string : wantCount.keySet()) {
-            if (!wantCount.get(string).equals(curCount.getOrDefault(string, 0))) {
-                return false;
-            }
-        }
-        return true;
+    private boolean isPossible(Set<Integer> set) {
+        return set.stream().allMatch(i -> i == 0);
     }
 }
