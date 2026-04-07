8dc24b57ab7a971430dad9132099afe183873523
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-04-08 08:40:48 +0900

    [Silver III] Title: N과 M (7), Time: 644 ms, Memory: 128716 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int N;
+    static int M;
+    static int[] array;
+    static StringBuilder sb = new StringBuilder();
+    static StringBuilder answer = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        array = new int[N];
+        int index = 0;
+
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreElements()) {
+            array[index++] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(array);
+
+        for (int i = 0; i < N; i++) {
+            sb.append(array[i]).append(" ");
+            func(1);
+            sb.delete(sb.length() - 1, sb.length());
+            sb.delete(sb.lastIndexOf(" ") + 1, sb.length());
+        }
+
+        System.out.println(answer);
+    }
+
+    private static void func(int count) {
+        if (count == M) {
+            answer.append(sb).append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            sb.append(array[i]).append(" ");
+            func(count + 1);
+            sb.delete(sb.length() - 1, sb.length());
+            sb.delete(sb.lastIndexOf(" ") + 1, sb.length());
+        }
+    }
+}
