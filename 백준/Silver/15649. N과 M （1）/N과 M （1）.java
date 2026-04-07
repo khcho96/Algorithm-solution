46bbec850e4b00d73d4e8f6353d31f243db2765c
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-04-07 20:54:00 +0900

    [Silver III] Title: N과 M (1), Time: 780 ms, Memory: 74144 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int N;
+    static int M;
+    static boolean[] visit;
+    static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visit = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            visit[i] = true;
+            list.add(i);
+            func(i);
+            list.remove(list.size() - 1);
+            visit[i] = false;
+        }
+    }
+
+    private static void func(int cur) {
+        if (list.size() == M) {
+            System.out.println(String.join(" ", list.stream().map(String::valueOf).toArray(String[]::new)));
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                list.add(i);
+                func(i);
+                list.remove(list.size() - 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
