fd6743dd91e9886c93fc1058467c70661e271660
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-04-08 09:08:34 +0900

    [Silver II] Title: N과 M (10), Time: 104 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int N;
+    static int M;
+    static int[] array;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+    static Set<String> answer = new LinkedHashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        array = new int[N];
+        visit = new boolean[N];
+
+        int index = 0;
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreElements()) {
+            array[index++] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(array);
+
+        for (int i = 0; i < N; i++) {
+            sb.append(array[i]).append(" ");
+            visit[i] = true;
+            func(i, 1);
+            visit[i] = false;
+            sb.delete(sb.length() - 1, sb.length());
+            sb.delete(sb.lastIndexOf(" ") + 1, sb.length());
+        }
+
+        StringBuilder a = new StringBuilder();
+        for (String s : answer) {
+            a.append(s).append("\n");
+        }
+
+        System.out.println(a);
+    }
+
+    private static void func(int cur, int count) {
+        if (count == M) {
+            answer.add(String.valueOf(sb));
+            return;
+        }
+
+        for (int i = cur; i < N; i++) {
+            if (!visit[i]) {
+                sb.append(array[i]).append(" ");
+                visit[i] = true;
+                func(i, count + 1);
+                visit[i] = false;
+                sb.delete(sb.length() - 1, sb.length());
+                sb.delete(sb.lastIndexOf(" ") + 1, sb.length());
+            }
+        }
+    }
+}
