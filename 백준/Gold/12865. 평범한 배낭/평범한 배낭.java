03e61296682caee1a39dd27e0c13075877ca507f
Kyeonghyeon Cho <khcho1492@gmail.com>
2026-04-20 00:07:05 +0900

    [Gold V] Title: 평범한 배낭, Time: 240 ms, Memory: 94692 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static int N;
+    static int K;
+    static long[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        dp = new long[N + 1][K + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j <= K; j++) {
+                dp[i][j] = dp[i - 1][j];
+
+                if (j - w >= 0) dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - w] + v);
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+}
